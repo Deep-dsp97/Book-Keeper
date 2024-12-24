21e947e88ed969c4843b9eb48ee87f638705f005
Deepkumar Patel <deepthedeveloper97@gmail.com>
2024-12-24 09:30:08 +0530

    Added Required Providers to check if it works properly to refactor the app in the next step

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,12 +1,20 @@
+import { useContext } from "react";
+import BooksContext from '../context/books.jsx';
 import BookShow from "./BookShow";
 
 const BookList = ({books, deleteBook, onEdit}) => {
+
+  const {count, incrementCount} = useContext(BooksContext);
 
   const renderedBooks = books.map((book) => {
     return <BookShow book={book} key={book.id} deleteBook={deleteBook} onEdit={onEdit}/>
   })
     return (
-      <div className="book-list">{renderedBooks}</div>
+      <div className="book-list">
+        {count}
+        <button onClick={incrementCount}>Click!</button>
+        {renderedBooks}
+      </div>
     )
   }
   
